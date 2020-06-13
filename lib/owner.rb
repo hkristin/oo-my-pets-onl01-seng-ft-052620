02c1db9205429eac26fb14ef8fa8d48880afcf1c
Kristin Howard <hkristin@andrew.cmu.edu>
2020-06-13 02:06:57 +0000

    Automatically backed up by Learn

--- lib/owner.rb
+++ lib/owner.rb
@@ -1,3 +1,42 @@
 class Owner
-  # code goes here
+  attr_accessor :cats, :dogs
+  attr_reader :name, :species
+  
+  @@all = []
+  
+  def initialize(name, species = "human")
+    @name = name
+    @species = species
+    @@all << self
+    @cats = []
+    @dogs = []
+  end
+  
+  def species
+    "human"
+  end
+
+  def say_species
+    return "I am a human."
+  end
+  
+  def self.all 
+    @@all
+  end
+  
+  def self.count
+    @@all.count
+  end
+  
+  def self.reset_all
+    @@all.clear
+  end
+  
+  def cats
+    self.cats
+  end
+  
+  def dogs
+    self.dogs
+  end
 end
